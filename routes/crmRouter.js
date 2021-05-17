6d45804e9b5a3af4bc8588bb13d50904f13fcce5
linc <645164947@qq.com>
2021-05-17 18:16:24 +0800

    Update crmRouter.js

--- routes/crmRouter.js
+++ routes/crmRouter.js
@@ -15,7 +15,7 @@ router.post('/api/article/upload', upload.txcos, ArticleController.createUpload)
 router.put('/api/article/articlePut', ArticleController.articlePut);                      // 编辑文章
 router.delete('/api/article/delArticle', ArticleController.delArticle);                   // 删除文章
 router.delete('/api/article/delComment', ArticleController.delComment);                   // 删除评论
-
+ 
 
 
 
